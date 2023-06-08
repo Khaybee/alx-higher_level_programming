aacf770a9d56f8dceddecafb4123696c79b78657
Khaybee <kachieosuji11@gmail.com>
2023-06-07 19:32:46 -0700

    Task 7

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def islower(c):
+    #check for lowercase characters
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False    
