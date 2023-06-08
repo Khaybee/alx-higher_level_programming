393845b1f92bf7b77d53458d4ef5260d5a78f145
Khaybee <kachieosuji11@gmail.com>
2023-06-07 20:00:13 -0700

    Updated task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 # Get the remainder of the absolute number when divided by 10
 
 def print_last_digit(number):
-    last_digit = (abs(number) % 10) 
+    last_digit = (abs(number) % 10)
     print(last_digit, end="")
     return last_digit
