a9eb0ad45144d099d5692b41369e50b1151f44eb
Khaybee <kachieosuji11@gmail.com>
2023-06-14 20:54:47 +0000

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,4 +7,4 @@ def weight_average(my_list=[]):
     sum_product = sum(score * weight for score, weight in my_list)
     sum_weights = sum(weight for _, weight in my_list)
     
-    return sum_product / sum_weights
+    return (sum_product / sum_weights)
