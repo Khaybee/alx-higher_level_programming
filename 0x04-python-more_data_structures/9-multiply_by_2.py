d526fff5a37301d925cc829c5e1b70ec8c795a66
Khaybee <kachieosuji11@gmail.com>
2023-06-14 20:09:37 +0000

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_directory = a_dictionary.copy()
+    list_keys = list(new_directory.keys())
 
-def best_score(a_dictionary):
-	
+    for i in list_keys:
+        new_directory[i] *= 2
+
+    return (new_directory)
