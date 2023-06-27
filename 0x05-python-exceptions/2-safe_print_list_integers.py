7775c607c940b2b885c9acdfc8f7fcc1a8692fdb
Khaybee <kachieosuji11@gmail.com>
2023-06-27 12:20:51 +0000

    2-safe

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+	count = 0
+	try:
+		for value in my_list:
+			if isinstance(value, int):
+				print("{:d}".format(value), end="")
+				count += 1
+			if count == x:
+				break
+		print()
+		return count
+	except TypeError:
+		return count
