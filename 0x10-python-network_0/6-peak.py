39fec7987a8bd2103002c14aade7355c053cc00c
Khaybee <kachieosuji11@gmail.com>
2024-01-25 17:40:17 -0800

    task 6 update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""Algorithms for list of integers."""
 
 
-def find_peak(lst):
-    """Finds a peak in lst"""
-
-    if lst is None or lst == []:
-        return None
-    low = 0
-    high = len(lst)
-    mid = ((high - low) // 2) + low
-    mid = int(mid)
-    if high == 1:
-        return lst[0]
-    if high == 2:
-        return max(lst)
-    if lst[mid] >= lst[mid - 1] and lst[mid] >= lst[mid + 1]:
-        return lst[mid]
-    if mid > 0 and lst[mid] < lst[mid + 1]:
-        return find_peak(lst[mid:])
-    if mid > 0 and lst[mid] < lst[mid - 1]:
-        return find_peak(lst[:mid])
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
